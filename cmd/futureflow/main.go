95efe6aa184c79eae3b88b44c2333192c0689386
Matsu <124514952+Muramatsuu@users.noreply.github.com>
2025-08-02 15:20:59 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/futureflow/main.go
@@ -0,0 +1,23 @@
+// cmd/futureflow/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"futureflow/internal/futureflow"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := futureflow.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
